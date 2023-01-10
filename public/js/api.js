96d57c0eaccef275d1795d91bafd7150c4e2292b
Otávio Berthouldo <73184375+otavioberthouldo@users.noreply.github.com>
2023-01-10 19:54:44 -0300

    Conclusão interface Netflix 3.0
    Webinar, Interface do Netflix com CSS, HTML, Javascript e Node

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = 'f20a6f40ccca8622aab58bd39fcf80c8'
+
+let img_url = 'https://image.tmdb.org/t/p/w500'
+
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+
+let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
